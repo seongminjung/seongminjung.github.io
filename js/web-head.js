c024ecc0947dbc54215164507e88d99c9bd61395
seongminjung <sung.minner@gmail.com>
2025-02-02 15:52:05 +0900

    Fix head tag order

--- js/web-head.js
+++ js/web-head.js
@@ -6,9 +6,12 @@ export class WebHead extends HTMLElement {
     super();
 
     const head = document.head;
+    const firstLinkOrScript = head.querySelector("link, script");
+    const fragment = document.createDocumentFragment();
+
     this.titleElement = document.createElement("title");
     this.setTitle();
-    head.appendChild(this.titleElement);
+    fragment.appendChild(this.titleElement);
 
     const metaTags = [
       { charset: "UTF-8" },
@@ -21,57 +24,52 @@ export class WebHead extends HTMLElement {
     metaTags.forEach((metaData) => {
       const meta = document.createElement("meta");
       Object.entries(metaData).forEach(([key, value]) => meta.setAttribute(key, value));
-      head.appendChild(meta);
+      fragment.appendChild(meta);
     });
 
-    const favicon = document.createElement("link");
-    favicon.rel = "shortcut icon";
-    favicon.href = "/favicon.png";
-    head.appendChild(favicon);
+    const resources = [
+      { tag: "link", rel: "shortcut icon", href: "/favicon.png" },
+      { tag: "script", async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-LL44K1WZ0G" },
+      {
+        tag: "script",
+        textContent: `
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag("js", new Date());
+gtag("config", "G-LL44K1WZ0G");
+      `,
+      },
+      {
+        tag: "script",
+        type: "text/x-mathjax-config",
+        textContent: `
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$', '$'], ['\\\\(', '\\\\)']]}
+});
+      `,
+      },
+      {
+        tag: "script",
+        async: true,
+        src: "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML",
+      },
+      {
+        tag: "link",
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+      },
+    ];
 
-    const gtagScript = document.createElement("script");
-    gtagScript.async = true;
-    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-LL44K1WZ0G";
-    head.appendChild(gtagScript);
-
-    const gtagInlineScript = document.createElement("script");
-    gtagInlineScript.textContent = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag("js", new Date());
-      gtag("config", "G-LL44K1WZ0G");
-    `;
-    head.appendChild(gtagInlineScript);
-
-    const mathjaxConfig = document.createElement("script");
-    mathjaxConfig.type = "text/x-mathjax-config";
-    mathjaxConfig.textContent = `
-      MathJax.Hub.Config({
-        tex2jax: {inlineMath: [['$', '$'], ['\\\\(', '\\\\)']]}
+    resources.forEach((res) => {
+      const element = document.createElement(res.tag);
+      Object.entries(res).forEach(([key, value]) => {
+        if (key !== "tag" && key !== "textContent") element.setAttribute(key, value);
       });
-    `;
-    head.appendChild(mathjaxConfig);
+      if (res.textContent) element.textContent = res.textContent;
+      fragment.appendChild(element);
+    });
 
-    const mathjaxScript = document.createElement("script");
-    mathjaxScript.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML";
-    mathjaxScript.async = true;
-    head.appendChild(mathjaxScript);
-
-    const fontAwesome = document.createElement("link");
-    fontAwesome.rel = "stylesheet";
-    fontAwesome.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-    head.appendChild(fontAwesome);
-
-    const css = document.createElement("link");
-    css.rel = "stylesheet";
-    css.href = "/css/index.css";
-    head.appendChild(css);
-
-    const mainJs = document.createElement("script");
-    mainJs.type = "module";
-    mainJs.src = "/js/main.js";
-    mainJs.defer = true;
-    head.appendChild(mainJs);
+    head.insertBefore(fragment, firstLinkOrScript);
   }
 
   setTitle() {
